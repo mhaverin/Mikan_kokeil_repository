f61de053360599a6fd114bdf3cfa7cf3bd07b233
mhaverin <46022230+mhaverin@users.noreply.github.com>
2018-12-21 11:04:22 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,19 @@ import App from "./App";
 
 Vue.config.productionTip = false;
 
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import { AmplifyPlugin } from 'aws-amplify-vue';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
+Vue.use(AmplifyPlugin, AmplifyModules);
+
+// It's important that you instantiate the Vue instance after calling Vue.use!
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app');
+
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
